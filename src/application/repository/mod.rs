0d5856dd650cb4f6ec4be4383ca0743f067faa61
Michel Philippe Edubas <mikeedubas@hotmail.com>
2026-05-04 08:58:32 +1000

    separated files for repositor generic trait

--- src/application/repository/mod.rs
+++ src/application/repository/mod.rs
@@ -1,3 +1,6 @@
+mod repository;
+pub use repository::Repository;
+
 pub mod activity_booking_repository;
 pub mod activity_repository;
 pub mod client_repository;
@@ -13,15 +16,3 @@ pub mod speaker_repository;
 pub mod sponsor_repository;
 pub mod user_repository;
 pub mod venue_repository;
-
-use crate::domain::error::DomainError;
-
-#[async_trait::async_trait]
-pub trait Repository<T>: Send + Sync {
-    async fn find_all(&self, offset: u32, limit: u32) -> Result<Vec<T>, DomainError>;
-    async fn find_by_id(&self, id: u64) -> Result<T, DomainError>;
-    async fn create(&self, entity: T) -> Result<T, DomainError>;
-    async fn update(&self, entity: T) -> Result<T, DomainError>;
-    async fn delete(&self, id: u64) -> Result<(), DomainError>;
-    async fn count(&self) -> Result<u64, DomainError>;
-}
